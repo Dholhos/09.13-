b918beb991431dce0cd4a6d5e09db7c335a5e988
Dholhos <113104720+Dholhos@users.noreply.github.com>
2023-09-15 12:51:56 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _09._13_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static List<Operator> operatorok;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                //lblEditor.Content = File.ReadAllText(openFileDialog.FileName);
+                FileBeolvas(openFileDialog.FileName);
+                lblDarab.Content = operatorok.Count();
+
+            }
+        }
+        public void FileBeolvas(string filename)
+        {
+            operatorok = new List<Operator>();
+            foreach (var sorok in File.ReadAllLines(filename))
+            {
+                int a = int.Parse(sorok.Split()[0]);
+                int c = int.Parse(sorok.Split()[2]);
+                string b = sorok.Split()[1];
+                operatorok.Add(new Operator(a, c, b));
+            }
+        }
+
+        private void btMaradek_Click(object sender, RoutedEventArgs e)
+        {
+            lblMaradekos.Content = operatorok.Count(x => x.Opertaor == "mod");
+        }
+
+        private void btDontes_Click(object sender, RoutedEventArgs e)
+        {
+            if (operatorok.Any(x => x.Operandus1 % 10 == 0))
+            {
+                if (operatorok.Any(x => x.Operandus2 % 10 == 0))
+                {
+                    lblVanE.Content = "Van ilyen kifejezés!";
+                }
+            }
+            else
+            {
+                lblVanE.Content = "Nincs ilyen kifejezés!";
+            }
+        }
+
+        private void btStat_Click(object sender, RoutedEventArgs e)
+        {
+            lbiEgy.Content = "mod -> " + operatorok.Count(x => x.Opertaor == "mod") + "db";
+            lbiKetto.Content = "/ -> " + operatorok.Count(x => x.Opertaor == "/") + "db";
+            lbiHarom.Content = "div -> " + operatorok.Count(x => x.Opertaor == "div") + "db";
+            lbiNegy.Content = "- -> " + operatorok.Count(x => x.Opertaor == "-") + "db";
+            lbiOt.Content = "* -> " + operatorok.Count(x => x.Opertaor == "*") + "db";
+            lbiHat.Content = "+ -> " + operatorok.Count(x => x.Opertaor == "+") + "db";
+
+        }
+    }
+}
---
+++ Operator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09._13_
+{
+    internal class Operator
+    {
+        int operandus1, operandus2;
+        string opertaor;
+
+        public Operator(int operandus1, int operandus2, string opertaor)
+        {
+            this.operandus1 = operandus1;
+            this.operandus2 = operandus2;
+            this.opertaor = opertaor;
+        }
+
+        public int Operandus1 { get => operandus1; set => operandus1 = value; }
+        public int Operandus2 { get => operandus2; set => operandus2 = value; }
+        public string Opertaor { get => opertaor; set => opertaor = value; }
+
+        
+    }
+}
